834a0f90cef40868e177a514129a5e534297590a
Hovakim Grabski <hovakim.grabski@gmail.com>
2025-05-12 18:13:08 +0400

    little update

--- bin/makePlots.py
+++ bin/makePlots.py
@@ -106,27 +106,62 @@ def start_program(input):
             curr_dataset = df[ df['_DATASET_'] == dataset]
 
             top_rank1 = curr_dataset[curr_dataset['RANK'] == 1]
-            logger.debug( " Debug> {}".format(top_rank1))
-
+            # logger.debug( " Debug> {}".format(top_rank1))
+            # logger.debug( " Debug> {}".format(top_rank1['rmsd_≤_2å']))
 
             # -- * Make plot how many are rmsd_≤_2å
+
 
             # Count the number of True and False values
             count_data = top_rank1['rmsd_≤_2å'].value_counts().reset_index()
             count_data.columns = ['rmsd_≤_2å', 'count']
+            # logger.debug( " Debug> {}".format(count_data) )
+
+            # Check if all values in the specified columns are True for each row
+            columns_of_interest=['mol_true_loaded',
+            'mol_cond_loaded', 'sanitization', 'inchi_convertible',
+            'all_atoms_connected', 'molecular_formula', 'molecular_bonds',
+            'double_bond_stereochemistry', 'tetrahedral_chirality', 'bond_lengths',
+            'bond_angles', 'internal_steric_clash', 'aromatic_ring_flatness',
+            'non-aromatic_ring_non-flatness', 'double_bond_flatness',
+            'internal_energy', 'protein-ligand_maximum_distance',
+            'minimum_distance_to_protein', 'minimum_distance_to_organic_cofactors',
+            'minimum_distance_to_inorganic_cofactors', 'minimum_distance_to_waters',
+            'volume_overlap_with_protein', 'volume_overlap_with_organic_cofactors',
+            'volume_overlap_with_inorganic_cofactors', 'volume_overlap_with_waters',
+            'rmsd_≤_2å']
+            top_rank1['all_true'] = top_rank1[columns_of_interest].all(axis=1)
+
+            # Count True or False values in the 'all_true' column
+            value_counts = top_rank1['all_true'].value_counts().reset_index()
+            value_counts.columns = ['rmsd_≤_2å_PB_VALID', 'count']
+
+
 
             # Add a percentage column
             total = count_data['count'].sum()
+            total_PB = value_counts['count'].sum()
             count_data['percentage'] = (count_data['count'] / total) * 100
+            count_data['PB_percentage'] = (value_counts['count'] / total_PB) * 100
+
+            # logger.debug(" Debug> {}".format(count_data))
+
+            # # Reshape data to long format
+            melted_data = count_data.melt(id_vars='rmsd_≤_2å', value_vars=['percentage', 'PB_percentage'], var_name='Type', value_name='Percentage')
+            logger.debug (" Debug> {}".format(melted_data))
+
 
             # Create the barplot
             plt.figure()
-            sns.barplot(data=count_data, x='rmsd_≤_2å', y='percentage')
+            # sns.barplot(data=count_data, x='rmsd_≤_2å', y='percentage')
+
+
+            sns.barplot(data=melted_data, x='rmsd_≤_2å', y='Percentage', hue='Type')
 
             # Add labels and show the plot
-            plt.xlabel('rmsd_≤_2å')
+            plt.xlabel('RMSD ≤ 2Å')
             plt.ylabel('Percentage')
-            plt.title('Count of True/False in rmsd_≤_2å')
+            plt.title('Count of True/False in RMSD ≤ 2Å')
 
             output = 'output_{}.svg'.format(dataset)
             outputpdf = 'output_{}.pdf'.format(dataset)
@@ -136,6 +171,7 @@ def start_program(input):
 
             plt.savefig(outputpdf)
             plt.gca()
+            logger.debug(" ==="*20)
 
 
         logger.info(" Info> There were no errorrmsd_≤_2ås")
